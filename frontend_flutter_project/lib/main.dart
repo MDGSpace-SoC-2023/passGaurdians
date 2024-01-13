6159bb711504bc19e0724b39acca824eebbd9f01
madhupriyakm <madhupriyakmcta577@gmail.com>
2024-01-13 14:13:27 +0530

    feat adds password generator

--- frontend_flutter_project/lib/main.dart
+++ frontend_flutter_project/lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 //import 'package:loginuicolors/password_details.dart';
-import 'login.dart';
-import 'register.dart';
-import 'homepage.dart';
-=======
 import 'screens/login.dart';
 import 'screens/register.dart';
 import 'screens/homepage.dart';
->>>>>>> fc626d25db8a15c9f7c6b9bc110fd411d1c75f1c
 
 void main() {
   runApp(MaterialApp(
